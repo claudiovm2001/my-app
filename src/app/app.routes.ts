726a781dca33f42dfb7488de53e052c2d4be00dd
Cláudio Vitor <claudiovitor2001@gmail.com>
2023-11-25 17:27:12 -0300

    criada navbar, criados esqueletos de paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { ListComponent } from './list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: RegisterComponent},
+    {path: 'list', component: ListComponent}
+];
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+}
